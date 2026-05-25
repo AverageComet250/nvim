6ce23fa75489d83632a799021558f53a7a65ce2b
Shravan Mandava <shravan@mandava8.com>
2026-05-25 10:35:31 +0100

    Disable softwrapping

--- lua/options.lua
+++ lua/options.lua
@@ -6,5 +6,6 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.smarttab = true
+vim.opt.wrap = false
 
 vim.opt.clipboard = vim.env.SSH_CONNECTION and "" or "unnamedplus"
