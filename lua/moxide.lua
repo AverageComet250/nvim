f5a0a2f5a56110ed3822a1b2c177c8c768a9176b
Shravan Mandava <shravan@mandava8.com>
2026-07-15 15:43:59 +0100

    Add markdown.oxide LSP daily command

--- lua/moxide.lua
+++ lua/moxide.lua
@@ -9,6 +9,21 @@ vim.lsp.config('markdown_oxide', {
     capabilities = capabilities,
 })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if not client or client.name ~= "markdown_oxide" then
+            return
+        end
+        if vim.fn.exists(":Daily") == 0 then
+            vim.api.nvim_create_user_command("Daily", function(cmd_args)
+                local target = cmd_args.args ~= "" and cmd_args.args or "today"
+                client:exec_cmd({command="jump", arguments={target}})
+            end, { desc = "Open daily note", nargs = "*" })
+        end
+    end
+})
+
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
 
 vim.lsp.enable('markdown_oxide')
