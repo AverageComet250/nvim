735f3528c64879666a0ee2c25cc48d292568ac16
Shravan Mandava <shravan@mandava8.com>
2026-08-31 04:08:39 +0100

    rewrite kitty integration
    This is a from-scratch rewrite of this file w/ some actual thought put
    in. It's still not actually perfect though.
    
    There isn't really a good way determine if this is kitty.
    
    - Neovide inherits $TERM
    - Neovide inherits $KITTY_WINDOW_ID
    - Querying OSC through the UI is fuckin painful. It's just not worth
      trying to do
    
    So instead I just make sure it's not neovide. This is fine, because it's
    the only UI I ever use.
    
    There also isn't a good way to query the current font size. You _should_
    just use OSC, but as previously mentioned that's not a great option. I
    may at some point experiment w/ OSC 66 for setting the font size rather
    than the rc protocol so shells in other tabs can keep my normal terminal
    font size, but that's... not really the way you're supposed to do that.
    
    OSC sucks because there's no good way to read returned escape codes - I
    can write escape codes to the UI buffer pretty easily using
    `vim.api.nvim_ui_send()` but you have to register a handler if you want
    the response.
    
    If OSC worked then this entire thing would be a hell of a lot easer
    since I could check the terminal and then get the 3 variables I want w/
    just escape codes, as shown w/ `kitten query-terminal` (This fails when
    ran in `vim.system()` or `vim.fn.system()`)
    
    Anyway, as a temporary measure the script just tells kitty to reset the
    font size to whatever it thinks default is.

--- lua/kitty.lua
+++ lua/kitty.lua
@@ -1,49 +1,35 @@
-local font_inc = 4
+local bg, opacity
 
--- TODO: use vim.g.terminal_color_* where possible, as well as other global vars
+local fs = 14
 
-if vim.env.TERM == "xterm-kitty" then
-    local termbg
-    vim.system({
-        "kitten", "@", "get-colors"
-    }, { text = true }, function(obj)
-        for line in obj.stdout:gmatch("[^\r\n]+") do
-            local color = line:match("^background%s+(%S+)")
-            if color then termbg = color end
+if vim.env.KITTY_WINDOW_ID and !vim.g.neovide then
+    vim.system({ "kitten", "@", "ls" }, { text = true }, function(res)
+        local windows = vim.json.decode(res.stdout)
+        if #windows == 1 then
+            opacity = windows[1].background_opacity
         end
     end)
 
+    vim.system({ "kitten", "@", "get-colors" }, { text = true }, function(obj)
+        for line in obj.stdout:gmatch("[^\r\n]+") do
+            local color = line:match("^background%s+(%S+)")
+            if color then bg = color end
+        end
+    end)
 
-    vim.api.nvim_create_autocmd("VimLeavePre", {
-        callback = function()
-            local opacity
-            vim.system({
-              "rg",
-              "^background_opacity\\s+([0-9.]+)",
-              "-r",
-              "$1",
-              vim.fn.expand("~/.config/kitty/kitty.conf"),
-            }, { text = true }, function(obj)
-            end)
+    vim.system({ "kitten", "@", "set-background-opacity", "1" }, { text = true }, function() end)
+    vim.system({ "kitten", "@", "set-font-size", "--", tostring(fs) }, { text = true }, function() end)
+    vim.system({ "kitten", "@", "set-color", "background=" .. string.format("#%06x", vim.api.nvim_get_hl(0, { name = "Normal" }).bg) }, { text = false }, function() end)
 
-            -- vim.system({ "kitten", "@", "set-background-opacity", opacity }, { text = true }, function() end)
-            vim.system({ "kitten", "@", "set-background-opacity", 0.5 }, { text = true }, function() end)
-            vim.system({ "kitten", "@", "set-font-size", "--", "-" .. font_inc }, { text = true }, function() end)
-            vim.system({ "kitten", "@", "set-color", "background=" .. termbg }, { text = false }, function() end)
+    vim.api.nvim_create_autocmd('VimLeavePre', { callback = function()
+        -- these should be vim.uv.spawn() calls w/ { detached = true }
+        vim.system({ "kitten", "@", "set-background-opacity", opacity }, { text = true }, function() end)
+        vim.system({ "kitten", "@", "set-font-size", "--", "0" }, { text = true }, function() end)
+        vim.system({ "kitten", "@", "set-color", "background=" .. bg }, { text = false }, function() end)
+    end})
+
+    vim.api.nvim_create_autocmd("ColorScheme", { callback = function()
+            vim.system({ "kitten", "@", "set-color", "background=" .. string.format("#%06x", vim.api.nvim_get_hl(0, { name = "Normal" }).bg) }, { text = false }, function() end)
         end,
     })
-
-    local bg = string.format("#%06x", vim.api.nvim_get_hl(0, { name = "Normal" }).bg)
-
-    vim.api.nvim_create_autocmd("ColorScheme", {
-        callback = function()
-            bg = string.format("#%06x", vim.api.nvim_get_hl(0, { name = "Normal" }).bg)
-
-            vim.system({ "kitten", "@", "set-color", "background=" .. bg }, { text = false }, function() end)
-        end,
-    })
-
-    vim.system({ "kitten", "@", "set-background-opacity", 1 }, { text = true }, function() end)
-    vim.system({ "kitten", "@", "set-font-size", "--", "+" .. font_inc }, { text = true }, function() end)
-    vim.system({ "kitten", "@", "set-color", "background=" .. bg }, { text = false }, function() end)
 end
